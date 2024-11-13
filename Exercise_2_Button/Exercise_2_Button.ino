ed16ede77a78a93c6db76a13448ea19e5506f688
Syed Rehman <137815407+SyedwRehman@users.noreply.github.com>
2024-11-13 12:45:26 -0500

    Add files via upload

--- Exercise_2_Button/Exercise_2_Button.ino
+++ Exercise_2_Button/Exercise_2_Button.ino
@@ -10,16 +10,16 @@
 //Debouncing from the button and how to fix it
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;  // the number of the pushbutton pin
-const int ledPin = 13;    // the number of the LED pin
+const int buttonPin = 2;  // the Port number of the pushbutton pin
+const int ledPin = 13;    // the Port number of the LED pin
 
 // Variables will change:
 int ledState = LOW;        // the current state of the output pin
 int buttonState;            // the current reading from the input pin
 int lastButtonState = LOW;  // the previous reading from the input pin
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
+// the following variables are unsigned longs because the time, measured in milliseconds, 
+// will quickly become a large value which can be stored in an int.
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
